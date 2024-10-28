b0c32ae1439db53ca31ff42b6e71a48a746efe7b
Hiroshi IKEGAMI <hiroshi.ikegami@magicdrive.jp>
2024-10-29 02:12:18 +0900

    version string

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
-import cmd "github.com/magicdrive/kirke/cmd/kirke"
+import (
+	"runtime/debug"
+
+	cmd "github.com/magicdrive/kirke/cmd/kirke"
+)
 
 var version string
 
 func main() {
-	cmd.Excecute(version)
+	cmd.Excecute(Version())
+}
+
+func Version() string {
+	if version != "" {
+		return version
+	}
+
+	if buildInfo, ok := debug.ReadBuildInfo(); ok {
+		return buildInfo.Main.Version
+	}
+	return "unknown"
+
 }
